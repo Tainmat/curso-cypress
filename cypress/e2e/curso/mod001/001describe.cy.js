3041db5fe793f24dedefe57f00db7ab3dcfe2d83
Tainã Pino <tmartins.pino@gmail.com>
2024-07-10 16:55:41 -0300

    first commit

---
+++ cypress/e2e/curso/mod001/001describe.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+
+
+describe('Should group test', () => {
+  describe(' A specific test group', () => {
+    it('A internal test 1', () => {
+
+    })
+
+    it('A internal test 2', () => {
+
+    })
+  })
+
+  it('A internal test', () => {
+
+  })
+})
